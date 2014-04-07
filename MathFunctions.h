0b4d5e30658968b47a1ab3753d0d6003d40362de
Ming-der Wang <mingder78@yahoo.com>
2014-04-07 23:40:25 +0800

    init

---
+++ MathFunctions.h
@@ -0,0 +1,19 @@
+//
+//  MathFunctions.h
+//  ICodeMathUtils
+//
+//  Created by Brandon Trebitowski on 4/7/11.
+//  Copyright 2011 ELC Technologies. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface MathFunctions : NSObject {
+
+}
+
+- (NSArray *) fibonacci:(NSInteger) n;
+- (NSInteger) factorial:(NSInteger) n;
+
+@end
